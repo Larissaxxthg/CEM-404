d257a5f19451521a5b3a5073af6682a97cdc6356
Julia <juliaromas2007@gmail.com>
2024-11-28 12:35:11 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+window.sr=ScrollReveal({reset:true});
+
+sr.reveal('h1',{rotate:{x:0,y:70,z:0},duration:2000});
+sr.reveal('h2',{rotate:{x:0,y:70,z:0},duration:2000});
+sr.reveal('img',{rotate:{x:0,y:70,z:0},duration:2000});
+sr.reveal('p',{rotate:{x:0,y:70,z:0},duration:2000});
